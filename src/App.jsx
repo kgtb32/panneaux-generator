d71ce683c6d69b8546e0d4b4b5faa58855a559ec
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2023-02-11 19:17:55 +0100

    Add Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import RoadSign from "./pages/RoadSign";
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/roadsign" element={<RoadSign />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
