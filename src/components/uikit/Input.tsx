4848f7711fdb20f3e5eccd3f284188a474337aba
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2023-03-01 14:13:47 +0100

    Add Input in UI Kit

---
+++ src/components/uikit/Input.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Input(
+  props: React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  >
+) {
+  return <input className="input" {...props} />;
+}
