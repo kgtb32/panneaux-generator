80323f87e7da3501dd8690a992ff25e571c5b807
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2023-03-01 13:57:14 +0100

    Add uikit style css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "./styles/index.css";
+import "./static/css/index.css";
+import "./static/css/uikit.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
