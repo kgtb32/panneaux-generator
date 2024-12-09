c251370a18373eaf75e96207347bc6f3724b9be0
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2024-12-09 13:48:18 +0100

    Configured primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +16,14 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
