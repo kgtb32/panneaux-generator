6f642d38325e1825c03e485425bdd3f14ecf180e
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2026-04-20 22:42:32 +0200

    Hotfix changelog

--- src/app/env/env.ts
+++ src/app/env/env.ts
@@ -3,4 +3,8 @@ export default {
     linkedin: 'https://fr.linkedin.com/in/kevin-gomes-t',
     github: 'https://github.com/kgtb32',
   },
+  app: {
+    isLocal: window.location.href.includes('localhost'),
+    baseStaticUrl: 'https://kgtb32.github.io/panneaux-generator/',
+  },
 };
--- src/app/services/header/changelog.service.ts
+++ src/app/services/header/changelog.service.ts
@@ -2,14 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Changelog } from '../../models/changelog';
 import { Observable } from 'rxjs';
+import env from '../../env/env';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChangelogService {
+  private readonly baseUrl = env.app.isLocal ? '/' : env.app.baseStaticUrl;
   private readonly httpClient = inject(HttpClient);
 
   public getChangelog(): Observable<Changelog[]> {
-    return this.httpClient.get<Changelog[]>('/changelog.json');
+    return this.httpClient.get<Changelog[]>(`${this.baseUrl}changelog.json`);
   }
 }
