1c67e17b1ae380407addd67e487f8d6f483edf88
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2026-04-25 16:28:22 +0200

    Add collapse button in sign configuration menu

--- src/app/base-ui/sign-configure-menu/sign-configure-menu.component.ts
+++ src/app/base-ui/sign-configure-menu/sign-configure-menu.component.ts
@@ -9,14 +9,17 @@ import { SignArrowComponent } from '../sign-arrow/sign-arrow.component';
 import { Divider } from 'primeng/divider';
 import { SignArrowGroupComponent } from '../sign-arrow-group/sign-arrow-group.component';
 import { SignPartComponent } from '../sign-part/sign-part.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-    selector: 'app-sign-configure-menu',
-    templateUrl: './sign-configure-menu.component.html',
-    styleUrl: './sign-configure-menu.component.scss',
-    imports: [SignSizeComponent, SignTypeComponent, IdentificationComponent, InputNumber, FormsModule, SignArrowComponent, Divider, SignArrowGroupComponent, SignPartComponent]
+  selector: 'app-sign-configure-menu',
+  templateUrl: './sign-configure-menu.component.html',
+  styleUrl: './sign-configure-menu.component.scss',
+  imports: [SignSizeComponent, SignTypeComponent, IdentificationComponent, InputNumber, FormsModule, SignArrowComponent, Divider, SignArrowGroupComponent, SignPartComponent, ButtonModule]
 })
 export class SignConfigureMenuComponent {
   @Input({ required: true })
   public sign!: Sign;
+
+  public collapsed: boolean = false;
 }
